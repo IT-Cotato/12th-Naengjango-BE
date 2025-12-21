d3c72f1f3f8cb052efd3a46653aa2240c188f4d2
junjunseo <jun85862@gmail.com>
2025-12-21 19:34:12 +0900

    feat:공통 응답

---
+++ src/main/java/com/itcotato/naengjango/global/entity/BaseEntity.java
@@ -0,0 +1,32 @@
+package com.itcotato.naengjango.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+
+/**
+ * 모든 Entity의 Base 클래스
+ * createdAt, updatedAt 자동 관리
+ * 다른 엔티티에서 implements 해서 사용
+ */
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+}
