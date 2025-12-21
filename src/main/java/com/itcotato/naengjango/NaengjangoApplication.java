065c43d315263b4bc9c69104741b0b78050f8448
junjunseo <jun85862@gmail.com>
2025-12-21 18:51:56 +0900

    Initial commit

---
+++ src/main/java/com/itcotato/naengjango/NaengjangoApplication.java
@@ -0,0 +1,13 @@
+package com.itcotato.naengjango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaengjangoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaengjangoApplication.class, args);
+	}
+
+}
