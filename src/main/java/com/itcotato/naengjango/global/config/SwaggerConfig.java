c1d3f0a28e6b04a87d3f5cba5dbdd06d132372fd
junjunseo <jun85862@gmail.com>
2026-01-30 20:37:13 +0900

    feat: swagger 요청을 https로 보내는 걸로 변경

--- src/main/java/com/itcotato/naengjango/global/config/SwaggerConfig.java
+++ src/main/java/com/itcotato/naengjango/global/config/SwaggerConfig.java
@@ -4,8 +4,11 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 @SecurityScheme(
@@ -21,6 +24,12 @@ public class SwaggerConfig {
         Info info = new Info().title("Naengjango API").description("냉잔고 백엔드 API 문서").version("v1.0.0");
 
         return new OpenAPI()
-                .info(info);
+                .info(info)
+                .servers(List.of(
+                        new Server()
+                                .url("https://15.134.213.116.nip.io")
+                                .description("Production server")
+                ));
+
     }
 }
